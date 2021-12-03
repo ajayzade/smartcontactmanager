1c503737a88c20102aed6527ca771c7bc6fc513b
ajayzade <zadeajay2018@gmail.com>
2021-12-03 13:31:42 +0530

    first commit

---
+++ src/main/java/com/example/manager/config/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.manager.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.manager.dao.UserRepository;
+import com.example.manager.entities.User;
+
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		// fetching user from database
+
+		User user = userRepository.getUserByUserName(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not found user !!");
+		}
+
+		CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+		return customUserDetails;
+	}
+
+}
+
